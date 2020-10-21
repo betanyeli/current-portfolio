755792cd1fcd22b8c36514999584589c46fd215a
betanyeli <betangelii@gmail.com>
2020-10-20 22:46:34 -0300

    Updates

---
+++ precache-manifest.ac28a0e8f4f02595c119d29da61ffd06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3553345cdd2f4365a0b57e83b504a1",
+    "url": "/current-portfolio/index.html"
+  },
+  {
+    "revision": "c49b130fe7e357565c4e",
+    "url": "/current-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e27000221be844a887bc",
+    "url": "/current-portfolio/static/js/2.db4391dc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/current-portfolio/static/js/2.db4391dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49b130fe7e357565c4e",
+    "url": "/current-portfolio/static/js/main.c734fcab.chunk.js"
+  },
+  {
+    "revision": "adff303bb7eab2e3e570",
+    "url": "/current-portfolio/static/js/runtime-main.525b6559.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/current-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
